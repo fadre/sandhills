3286755b7af7d55a5766a84c983bf21cf8d007b2
dreier-fabian <dreier.fabian@gmail.com>
2016-02-28 17:24:05 +0100

    Allow Color Configuration, write to results directory

--- src/main/java/ch/fadre/sandhills/ImageWriter.java
+++ src/main/java/ch/fadre/sandhills/ImageWriter.java
@@ -6,8 +6,23 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageWriter {
+
+    private Map<Integer, Color> colorConfig = new HashMap<>();
+
+    public ImageWriter() {
+        colorConfig.put(0, new Color(32, 85, 126));
+        colorConfig.put(1, new Color(26, 151, 82));
+        colorConfig.put(2, new Color(224, 143, 18));
+        colorConfig.put(3, new Color(210, 43, 40));
+    }
+
+    public ImageWriter(Map<Integer, Color> colorConfig) {
+        this.colorConfig = colorConfig;
+    }
 
     public BufferedImage generateImage(int[][] grid, int width, int height) throws IOException, InterruptedException {
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
@@ -29,23 +44,19 @@ public class ImageWriter {
     }
 
     public void writeToFile(BufferedImage bufferedImage, String prefix) throws IOException {
-        String fileName = prefix + System.currentTimeMillis() + ".png";
-        ImageIO.write(bufferedImage, "png", new File(fileName));
+        String fileName = prefix  + ".png";
+        File resultsDir = new File("results");
+        if(!resultsDir.exists()){
+            resultsDir.mkdir();
+        }
+        ImageIO.write(bufferedImage, "png", new File(resultsDir,fileName));
     }
 
-    private static Color getColor(int i) {
-        switch (i) {
-            case 0:
-                return new Color(39, 111, 161);
-            case 1:
-                return new Color(26, 151, 82);
-            case 2:
-                return new Color(224, 143, 18);
-            case 3:
-                return new Color(176, 52, 39);
-            default:
-                System.out.println("This should never happen :)");
-                return new Color(39, 111, 161);
+    private Color getColor(int i) {
+        Color color = colorConfig.get(i);
+        if(color == null){
+            System.out.println("Warning: color not found for value" + i);
         }
+        return color;
     }
 }

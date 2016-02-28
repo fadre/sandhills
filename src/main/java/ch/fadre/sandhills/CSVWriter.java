9b2768158568ab843402fadc45b9c029a995515a
dreier-fabian <dreier.fabian@gmail.com>
2016-02-28 17:23:34 +0100

    Added Writer for CSV-Data

---
+++ src/main/java/ch/fadre/sandhills/CSVWriter.java
@@ -0,0 +1,23 @@
+package ch.fadre.sandhills;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CSVWriter {
+
+    public void writeToFile(int[][] grid, String prefix)  {
+        String fileName = prefix + System.currentTimeMillis() + ".csv";
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))){
+            for (int[] rows : grid) {
+                String line = IntStream.of(rows).boxed().map(String::valueOf).collect(Collectors.joining(";"));
+                bufferedWriter.write(line +"\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
